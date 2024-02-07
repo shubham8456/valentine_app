4f0026404cf6b1c286a0d05d7e9b957f3c25d0e3
shubham8456 <shubham8456@gmail.com>
2024-02-07 18:07:43 +0530

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import './App.css'
+import Manas from './assets/manas.jpeg'
+
+const phrases = [
+  "No 🙉",
+  "Are you sure?",
+  "Really Sure😢",
+  "I'll be very sad😕",
+  "Pookie Please🥺",
+  "Don't do this to me🫤",
+  "I'm gonna cry..😭",
+  "You are breaking my heart😭💔",
+]
+
+function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  
+  function handleYesClick() {
+    setYesPressed(true)
+  }
+  function handleNoClick() {
+    setNoCount(noCount+1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
+  return (
+    <div className='valentine-container'>
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="bear-Kissing" />
+          <div className='text'>Yayyy !!!</div>
+        </>
+      ) : (
+        <>
+          <img className='kiss' src={Manas} alt="Manaswi" />
+          <div className='text'>Hey Manaswi! Will you be my valentine 🌹?</div>
+          <div className='both-Button'>
+            <button className='yesButton'
+            style={{ fontSize: yesButtonSize , backgroundColor:'rgb(2, 117, 216)'}}
+              onClick={handleYesClick}
+            >
+              Yes 🙈
+            </button>
+            <button
+              className='NoButton'
+              style={{ }}
+              onClick={handleNoClick}
+            >
+              {getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
